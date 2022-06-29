2218ec3cc27fb1b1022077cfa4adcf3f85654917
Robert O'Brien <robrien@redhat.com>
2022-06-29 15:33:04 +0100

    Add autoheal and autohealconfig crds

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Autoheal-Operator
 
-An Operator based on the Operator SDK for automatically healing Prometheus alerts using scripts defined in an external repository.
+An Operator based on the Operator SDK for automatically healing Prometheus alerts using scripts defined in an external configuration repository.
 
 ## Prerequisites
-- [Operator-SDK](https://github.com/operator-framework/operator-sdk) v1.22.0
+- [Operator-SDK](https://v1-22-x.sdk.operatorframework.io/docs/building-operators/golang/installation/) v1.22.0
 - [Go](https://golang.org/dl/) v1.18
--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	autohealv1alpha1 "github.com/bf2fc6cc711aee1a0c2a/autoheal-operator/api/v1alpha1"
+	"github.com/bf2fc6cc711aee1a0c2a/autoheal-operator/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(autohealv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -85,6 +89,20 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.AutohealReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Autoheal")
+		os.Exit(1)
+	}
+	if err = (&controllers.AutohealConfigReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "AutohealConfig")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
